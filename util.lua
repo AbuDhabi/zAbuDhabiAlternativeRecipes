872d587b513b54f6dd5885b76396e8f4163f13e3
Abu Dhabi <adhabi@gmail.com>
2025-11-27 14:18:41 +0100

    Add debug flag.

--- util.lua
+++ util.lua
@@ -1,9 +1,14 @@
 local util = {}
 
+local DEBUG = false;
+
 -- Logs data to factorio-current.log using serpent for serialization.
 -- @param message: Optional prefix message
 -- @param data: The data to serialize and log
 function util.log_serpent(message, data)
+  if not DEBUG then
+    return;
+  end
   local serialized = serpent.block(data, {comment = false})
   if message then
     log(message .. "\n" .. serialized)
